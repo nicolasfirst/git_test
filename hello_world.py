00e1b194db55f6b570df7a21dad9fd28a95ea427
Nico Veenkamp <nico.veenkamp@gmail.com>
2021-05-31 13:54:04 +0200

    Test py

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# Dit is een pyhon programma bestand met de tekst
+# De bovenste regel en deze regel zijn commentaar
+
+# De regel hieronder drukt de tekst HELLO WORKD af
+# print() is het commando voor de afdruk functie.
+# Een tekst als HELLO WORLD moet tussen "" staan
+print ("HELLO WORLD")
+
+
